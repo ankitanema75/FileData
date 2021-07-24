ce07c8ffbff066289c6af2361c6bf2c5fe6e746d
Neeraj <Neeraj@NEERAJ>
2021-07-24 13:18:27 +0530

    Add project files.

---
+++ FileData/FileOperationHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    class FileOperationHelper
+    {
+        FileDetails fileDetailsObj = new FileDetails();
+        FileModel fileModelObj = new FileModel();
+        public FileModel getOutput(string fileArg, string filePath)
+        {
+            
+          
+           
+            try 
+            {
+                if(fileArg=="-v"||fileArg=="--v"||fileArg=="/v")
+                fileModelObj.Version= getVersion(filePath);
+
+               else if (fileArg == "-s" || fileArg == "--s" || fileArg == "/s")
+                    fileModelObj.Size = getSize(filePath);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return fileModelObj;
+        }
+
+        private int getSize(string arg2)
+        {
+           
+            int resultSize ;
+            
+            try
+            {
+                resultSize=fileDetailsObj.Size(arg2);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return resultSize;
+        }
+
+        private string getVersion(string arg)
+        {
+
+            string resultVersion = string.Empty;
+
+            try
+            {
+                resultVersion=fileDetailsObj.Version(arg);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return resultVersion;
+        }
+    }
+}
---
+++ FileData/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fileArg = string.Empty, filePath = string.Empty;
+         
+            FileOperationHelper foh = new FileOperationHelper();
+            FileModel fileModelObj = new FileModel();
+
+            if (args !=null)
+            {
+                fileArg = args[0];
+                filePath = args[1];
+            }
+            
+            fileModelObj=foh.getOutput(fileArg,filePath);
+
+            if (fileModelObj != null)
+            {
+                if(fileModelObj.Size==0)
+                    Console.WriteLine("Version: " + fileModelObj.Version);
+                else
+                    Console.WriteLine("Size " + fileModelObj.Size);
+            }
+
+
+            Console.ReadKey();
+
+        }
+
+      
+    }
+}
